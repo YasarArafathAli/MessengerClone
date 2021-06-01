fa4717e432e496636365285040e37034917ef173
Arafath <yasara218@gmail.com>
2021-06-02 00:34:26 +0530

    built complete

---
+++ precache-manifest.6ee19b620c136891a9429e792274748f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36334e21bc77118b6f65ae965ba92ca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a443e4bdee70a939059f",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "df5117c96ec793480a6a",
+    "url": "/static/css/main.906d9089.chunk.css"
+  },
+  {
+    "revision": "a443e4bdee70a939059f",
+    "url": "/static/js/2.424d3264.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.424d3264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5117c96ec793480a6a",
+    "url": "/static/js/main.73205572.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
